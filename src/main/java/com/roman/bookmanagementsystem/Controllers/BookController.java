8880527f6a3e08caddd76191040438585b802a7c
RomanYuen <romanyuen1001@gmail.com>
2024-07-10 16:22:56 +0800

    Updated annotation in controller

--- src/main/java/com/roman/bookmanagementsystem/Controllers/BookController.java
+++ src/main/java/com/roman/bookmanagementsystem/Controllers/BookController.java
@@ -2,6 +2,9 @@ package com.roman.bookmanagementsystem.Controllers;
 
 import com.roman.bookmanagementsystem.models.Book;
 import com.roman.bookmanagementsystem.services.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,20 +12,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("books")
+@RequiredArgsConstructor
 public class BookController {
-
+    @Autowired
     private final BookService bookService;
 
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         return "Welcome to Book Management System API";
     }
 
-    @RequestMapping()
+    @GetMapping()
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
