9464a6d7e09d81bf9dae6adeb67a4f502c26b5fa
RomanYuen <romanyuen1001@gmail.com>
2024-07-11 13:39:49 +0800

    Apply validateService to bookService and update business logic

--- src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
+++ src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
@@ -1,11 +1,13 @@
 package com.roman.bookmanagementsystem.dtos;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class BookDto {
-    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Title must be alphanumeric only") // TODO: Study validation
     public String title;
-    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Author must be alphanumeric only") // TODO: Study validation
     public String author;
     public Boolean published;
 }
