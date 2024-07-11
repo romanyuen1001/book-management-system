d3d597c5b42e9c3edf3d7c557ba28ce9ce5d0979
Roman Yuen <57783743+romanyuen1001@users.noreply.github.com>
2024-07-11 11:25:01 +0800

    Add api - updateBookPublishedStatus (#1)

--- src/main/java/com/roman/bookmanagementsystem/services/BookService.java
+++ src/main/java/com/roman/bookmanagementsystem/services/BookService.java
@@ -4,9 +4,11 @@ import com.roman.bookmanagementsystem.dtos.BookDto;
 import com.roman.bookmanagementsystem.models.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
     List<Book> findByAuthorAndPublished(String author, boolean published);
     void createBooks(List<BookDto> bookDto);
     void deleteBook(Long id);
+    Optional<Book> updateBookPublishedStatus(Long id, boolean published);
 }
