cd9011bedce3a7ee4fbfcb10954e54492be34214
RomanYuen <romanyuen1001@gmail.com>
2024-07-11 17:34:38 +0800

    Add unit tests for APIs

--- src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
+++ src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
@@ -1,11 +1,14 @@
 package com.roman.bookmanagementsystem.dtos;
 
-import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDto {
     public String title;
     public String author;
